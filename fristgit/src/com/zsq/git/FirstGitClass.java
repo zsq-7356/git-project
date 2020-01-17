5b42c9312f1c7b3ab9640492ce5317a8ff12bbe5
zsq <zsq@qq.com>
2020-01-17 16:55:25 +0800

    dd

---
+++ fristgit/src/com/zsq/git/FirstGitClass.java
@@ -0,0 +1,9 @@
+package com.zsq.git;
+
+public class FirstGitClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
